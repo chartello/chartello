2bc4a6c6e6ed5b77cd6bcc41eb8e0ac5ecbfead4
Matt Daneshvar <matt.daneshvar@gmail.com>
2023-02-04 21:38:32 -0600

    Remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Chartello\Lite\Http\Controllers\PanelsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::resource('dashboards', DashboardsController::class);
+Route::resource('dashboards', DashboardsController::class)->only(['show', 'update', 'store', 'destroy']);
 Route::resource('dashboards/{dashboard}/panels', DashboardPanelsController::class)->only('store');
-Route::resource('panels', PanelsController::class);
+Route::resource('panels', PanelsController::class)->only(['update', 'destroy']);
