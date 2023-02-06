b3f42965e4a771ee8eaf478360a26d72560e1b5b
Matt Daneshvar <matt.daneshvar@gmail.com>
2023-02-05 21:35:27 -0600

    Update comments to be consistent

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -6,25 +6,10 @@ use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'chartello:install';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Install all of the Chartello resources';
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
         $this->comment('Publishing Chartello Middleware...');
