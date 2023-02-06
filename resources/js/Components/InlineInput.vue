14914370fe1c3e58b1312731ad54733e9d868a24
Matt Daneshvar <matt.daneshvar@gmail.com>
2023-02-05 20:03:24 -0600

    Fix minor UI bugs

--- resources/js/Components/InlineInput.vue
+++ resources/js/Components/InlineInput.vue
@@ -1,14 +1,25 @@
 <template>
   <input
     :type="type"
-    class="border-0 p-0 underline decoration-gray-200 underline-offset-4 focus:decoration-blue-200 focus:ring-0"
+    ref="element"
+    class="border-0 bg-transparent p-0 underline decoration-gray-200 underline-offset-4 focus:decoration-blue-200 focus:ring-0"
   />
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 defineProps({
   type: {
     default: "text",
   },
 });
+
+const element = ref(null);
+
+function focus() {
+  element.value.focus();
+}
+
+defineExpose({ focus });
 </script>
