e298d53d5a44f5f23586fe05c62b110ea970695f
Matt Daneshvar <matt.daneshvar@gmail.com>
2023-02-16 20:10:50 -0600

    Mock up table panels

---
+++ resources/js/Components/Panels/Table.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="-mx-4 mb-4">
+    <table class="w-full text-left">
+      <thead>
+        <tr class="text-blue-400 grayscale-[0.3]">
+          <th class="px-4 py-3 pl-8 text-sm font-normal">ID</th>
+          <th class="px-4 py-3 text-sm font-normal">Name</th>
+          <th class="px-4 py-3 text-sm font-normal">Email</th>
+          <th class="px-4 py-3 pr-8 text-sm font-normal">Created At</th>
+        </tr>
+      </thead>
+      <tbody class="text-gray-500">
+        <tr class="border-t border-gray-100">
+          <td class="px-4 py-3 pl-8">2319</td>
+          <td class="px-4 py-3">Matt</td>
+          <td class="px-4 py-3">matt@example.com</td>
+          <td class="px-4 py-3 pr-8">2023-01-04</td>
+        </tr>
+        <tr class="border-t border-gray-100">
+          <td class="px-4 py-3 pl-8">2318</td>
+          <td class="px-4 py-3">John</td>
+          <td class="px-4 py-3">john@example.com</td>
+          <td class="px-4 py-3 pr-8">2023-01-04</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
