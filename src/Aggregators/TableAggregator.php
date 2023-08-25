c087fb13775418f0800c5d1d9ce0a89337b48eb7
Daniel, Petrica Andrei-Daniel <andrei@danielpetrica.com>
2023-08-25 11:16:08 +0200

    Update TableAggregator.php

--- src/Aggregators/TableAggregator.php
+++ src/Aggregators/TableAggregator.php
@@ -8,8 +8,8 @@ class TableAggregator extends BaseAggregator
 {
     public function get()
     {
-        DB::statement(DB::raw("SET @start := '{$this->start}'")->getValue(DB::connection()->getQueryGrammar()));
-        DB::statement(DB::raw("SET @end := '{$this->end}'")->getValue(DB::connection()->getQueryGrammar()));
+        DB::statement(DB::raw("SET @start = '{$this->start}'")->getValue(DB::connection()->getQueryGrammar()));
+        DB::statement(DB::raw("SET @end = '{$this->end}'")->getValue(DB::connection()->getQueryGrammar()));
 
         return DB::select($this->query);
     }
